409c26758972c483b4f175923e65d05d7725b3bb
Akshay23504 <aaravind@syr.edu>
2018-11-03 20:12:09 -0400

    Pre-processing kinda of complete. Features are neatly extracted in CSV files. I mean very very neatly. Given a raw dataset, we just need to change user id, sensor name and device. Just three lines and we get all features in cute CSV file

--- pt1.py
+++ pt1.py
@@ -13,15 +13,16 @@ def convert_epoch_to_datetime(time_in_epoch):
 
 class PreProcess:
     def __init__(self):
-        self.user_id = "500"
-        self.sensor_name = "gyroscope"
-        self.path = "../Dataset/RawFallLeft/" + self.user_id + "/"
-        self.filename = self.user_id + "_" + self.sensor_name + ".xlsx"
+        self.user_id = "505"
+        self.sensor_name = "accelerometer"  # accelerometer or gyroscope
+        self.device = "watch"  # phone or watch
+        self.path = "../Dataset/RawFallRight/" + self.user_id + "/"
+        self.filename = self.user_id + "_" + self.device + "_" + self.sensor_name + ".xlsx"
         self.filename_checkpoint = "Events" + self.user_id + ".txt"
-        self.door_open_checkpoints = {}
+        self.fall_checkpoints = []
         self.entries = {}
         self.window_size = 3  # In seconds
-        self.door_instance_dimension = {}
+        self.fall_instance_dimension = {}
 
     def store_checkpoints(self):
         """
@@ -31,77 +32,54 @@ class PreProcess:
 
         with open(self.path + self.filename_checkpoint) as input_file:
             read_input = csv.reader(input_file, delimiter=',')
-            door_counter = 1
             for row in read_input:
-                if row[0] == 'door open':
-                    if self.door_open_checkpoints.get(door_counter) is None:
-                        self.door_open_checkpoints[door_counter] = []
-                    self.door_open_checkpoints[door_counter].append(convert_epoch_to_datetime(float(row[1])))
-                if row[0] == 'event1':
-                    door_counter += 1
+                if row[0] == 'event2':  # Just making sure we consider only the events
+                    self.fall_checkpoints.append(convert_epoch_to_datetime(float(row[1])))
 
-    def store_door_opening_instances(self):
+    def store_fall_instances(self):
         """
-        The code is not really optimized for time complexity!
+        The code is not really optimized for time complexity! Pffff
         Maybe there is a better way..hmmm...
-
-        The jth loop is the instance. The instance starts from 0. This instance is
-        added as the first column to the entries
 
         """
 
         input_file = pd.ExcelFile(self.path + self.filename).parse('Sheet1').get_values()  # Sheet 1 only
         for row in input_file:
             time = convert_epoch_to_datetime(float(row[4]))
-            for i in range(len(self.door_open_checkpoints)):
-                for j in range(len(self.door_open_checkpoints[i + 1])):  # Ewwww....3 loops
-                    if (self.door_open_checkpoints[i + 1][j] - datetime.timedelta(seconds=self.window_size)) \
-                            <= time <= \
-                            (self.door_open_checkpoints[i + 1][j] + datetime.timedelta(seconds=self.window_size)):
-                        if self.entries.get(i + 1) is None:
-                            self.entries[i + 1] = {}
-                            self.entries[i + 1][j] = []
-                        if self.entries.get(i + 1).get(j) is None:
-                            self.entries.get(i + 1)[j] = []
-                        self.entries[i + 1][j].append([j, row[1], row[2], row[3], row[4]])
+            for i in range(len(self.fall_checkpoints)):
+                if self.fall_checkpoints[i] <= time <= \
+                        self.fall_checkpoints[i] + datetime.timedelta(seconds=self.window_size):
+                    if self.entries.get(i + 1) is None:
+                        self.entries[i + 1] = []
+                    self.entries[i + 1].append([i + 1, row[1], row[2], row[3], row[4]])
 
-    def get_door_opening_instances(self):
+    def get_fall_instances(self):
         """
-        door_instance_dimension is for each door, for each instance, there are
+        fall_instance_dimension is for each fall and there are
         x, y and z values
 
-        Example: door_instance_dimension[1][1]['x'] = -0.005
-        Door 1, instance 1, x value
+        Example: fall_instance_dimension[1]['x'] is fall instance 1, x value
 
         """
 
         for k, v in self.entries.items():
-            if self.door_instance_dimension.get(k) is None:
-                self.door_instance_dimension[k] = {}
-            for ks, vs in self.entries.get(k).items():
-                if self.door_instance_dimension.get(k).get(ks) is None:
-                    self.door_instance_dimension[k][ks] = {}
-                    self.door_instance_dimension[k][ks]['x'] = []
-                    self.door_instance_dimension[k][ks]['y'] = []
-                    self.door_instance_dimension[k][ks]['z'] = []
-                for vss in self.entries.get(k).get(ks):
-                    self.door_instance_dimension[k][ks]['x'].append(vss[1])
-                    self.door_instance_dimension[k][ks]['y'].append(vss[2])
-                    self.door_instance_dimension[k][ks]['z'].append(vss[3])
+            self.fall_instance_dimension[k] = {}
+            self.fall_instance_dimension[k]['x'] = []
+            self.fall_instance_dimension[k]['y'] = []
+            self.fall_instance_dimension[k]['z'] = []
+            for vs in v:
+                self.fall_instance_dimension[k]['x'].append(vs[1])
+                self.fall_instance_dimension[k]['y'].append(vs[2])
+                self.fall_instance_dimension[k]['z'].append(vs[3])
 
-    def write_entries_to_file(self):
-        # Code to put the results (entries) into CSV
-        for entry in self.entries:
-            with open(self.path + self.user_id + "_" + self.sensor_name + "_door" + str(entry) + ".csv", "w+") as o_file:
-                for e in self.entries[entry]:
-                    csv.writer(o_file).writerows(self.entries[entry][e])
+    def get_fall_instance_dimension(self):
+        return self.fall_instance_dimension
 
 
 # pre_process = PreProcess()
 #
 # pre_process.store_checkpoints()
-# pre_process.store_door_opening_instances()
-# pre_process.write_entries_to_file()
-# pre_process.get_door_opening_instances()
-
+# pre_process.store_fall_instances()
+# pre_process.get_fall_instances()
+#
 print()
---
+++ pt2.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as scs
+from statsmodels import robust
+
+import pt1
+
+
+class Feature:
+
+    def __init__(self, pre_process):
+        self.feature_list = []
+        self.label_list = ["Mean_x", "Median_x", "Variance_x", "Standard Deviance_x", "Median Absolute Deviation_x",
+                           "Interquartile Range_x", "Power_x", "Energy_x", "Peak-to-Peak Amplitude_x",
+                           "Auto Correlation_x", "Kurtosis_x", "Skew_x", "Entropy_x",
+                           "Mean_y", "Median_y", "Variance_y", "Standard Deviance_y", "Median Absolute Deviation_y",
+                           "Interquartile Range_y", "Power_y", "Energy_y", "Peak-to-Peak Amplitude_y",
+                           "Auto Correlation_y", "Kurtosis_y", "Skew_y", "Entropy_y",
+                           "Mean_z", "Median_z", "Variance_z", "Standard Deviance_z", "Median Absolute Deviation_z",
+                           "Interquartile Range_z", "Power_z", "Energy_z", "Peak-to-Peak Amplitude_z",
+                           "Auto Correlation_z", "Kurtosis_z", "Skew_z", "Entropy_z"
+                           ]
+        self.pre_process = pre_process
+        self.df = pd.DataFrame(columns=self.label_list)
+
+    def feature_mean(self, values):
+        self.feature_list.append(np.mean(values))
+
+    def feature_median(self, values):
+        self.feature_list.append(np.median(values))
+
+    def feature_variance(self, values):
+        self.feature_list.append(np.var(values))
+
+    def feature_standard_deviance(self, values):
+        self.feature_list.append(np.std(values))
+
+    def feature_median_absolute_deviation(self, values):
+        self.feature_list.append(robust.mad(values))
+
+    def feature_interquartile_range(self, values):
+        self.feature_list.append(scs.iqr(values))
+
+    def feature_power(self, values):
+        self.feature_list.append(sum(np.array(np.square(np.abs(values)))) / len(values))
+
+    def feature_energy(self, values):
+        self.feature_list.append(sum(np.array(np.square(np.abs(values)))))
+
+    def feature_peak_to_peak_amplitude(self, values):
+        self.feature_list.append(max(values) - min(values))
+
+    def feature_auto_correlation(self, values):
+        # result = np.correlate(values, values, mode='full')
+        # self.feature_list.append(result[int(result.size / 2):])
+        result = pd.Series(values)
+        self.feature_list.append(pd.Series.autocorr(result))
+
+    def feature_kurtosis(self, values):
+        self.feature_list.append(scs.kurtosis(values))
+
+    def feature_skew(self, values):
+        self.feature_list.append(scs.skew(values))
+
+    def feature_entropy(self, values):
+        self.feature_list.append(scs.entropy(values))
+
+    def get_me_all_features(self, values):
+        self.feature_mean(values)
+        self.feature_median(values)
+        self.feature_variance(values)
+        self.feature_standard_deviance(values)
+        self.feature_median_absolute_deviation(values)
+        self.feature_interquartile_range(values)
+        self.feature_power(values)
+        self.feature_energy(values)
+        self.feature_peak_to_peak_amplitude(values)
+        self.feature_auto_correlation(values)
+        self.feature_kurtosis(values)
+        self.feature_skew(values)
+        self.feature_entropy(values)
+
+    def some_pre_processing(self):
+        self.pre_process.store_checkpoints()
+        self.pre_process.store_fall_instances()
+        self.pre_process.get_fall_instances()
+
+    def construct_single_row(self, single_row_list, values):
+        self.feature_list = []
+        self.get_me_all_features(values)
+        single_row_list.append(self.feature_list)
+
+    def write_features_to_file(self):
+        self.some_pre_processing()
+        fall_instance_dimension = self.pre_process.get_fall_instance_dimension()
+        for k, v in fall_instance_dimension.items():
+            data_frame = self.df
+            single_row_values = []
+            self.construct_single_row(single_row_values, v['x'])
+            self.construct_single_row(single_row_values, v['y'])
+            self.construct_single_row(single_row_values, v['z'])
+            single_row_values = [val for row in single_row_values for val in row]
+            data_frame.loc["Instance_" + str(k)] = single_row_values
+            data_frame.to_csv(self.pre_process.path + self.pre_process.user_id + "_" + self.pre_process.device + "_" +
+                              self.pre_process.sensor_name + "_features.csv")
+
+
+f = Feature(pt1.PreProcess())
+f.write_features_to_file()
+
